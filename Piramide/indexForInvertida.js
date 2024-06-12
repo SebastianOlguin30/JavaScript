fe852dce27b75f1c0f05cf20a5b3878786e7fe37
Sebastian Olguin <sebastian.olguin@datco.cl>
2024-06-12 11:07:29 -0400

    Subiendo proyecto piramide

---
+++ Piramide/indexForInvertida.js
@@ -0,0 +1,20 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+//Funcion piramide 
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
---
+++ Piramide/indexWhile.js
@@ -0,0 +1,20 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+//Funcion piramide 
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+while (rows.length < count) {
+    rows.push(padRow(rows.length + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
